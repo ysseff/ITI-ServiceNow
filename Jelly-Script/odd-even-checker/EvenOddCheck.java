728a06f10ffb47eb22c902c817183af1f8ea9c71
Youssef Mahmoud <139473311+ysseff@users.noreply.github.com>
2026-02-25 13:56:14 +0200

    added jelly script task 3 and 4

---
+++ Jelly-Script/odd-even-checker/EvenOddCheck.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class EvenOddCheck {
+    public String check(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } 
+        else {
+            return "odd";
+        }
+    }
+}
